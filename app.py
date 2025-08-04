50e01922e32ba9af1f9f0d731588835afe687257
kt6312847-coder <kt6312847@gmail.com>
2025-08-04 13:38:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route("/voice", methods=['POST'])
 def voice():
     resp = VoiceResponse()
-    resp.say("مرحبا بك عند طه", language="ar-SA")
-    return Response(str(resp), mimetype="application/xml")
+    resp.say("مرحبًا بك عند طه. شكرًا لاتصالك.", language="ar-XA", voice="Polly.Zeina")
+    return Response(str(resp), mimetype='text/xml')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=False, host='0.0.0.0', port=10000)
 
